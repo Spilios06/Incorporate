7cafed9706934e43d19832c101eba9863039151a
Spilios Maderakis <smaderakis06@gmail.com>
2024-06-10 12:36:30 +0300

    Zoom In & Out

--- Assets/Scripts/ComputerItem.cs
+++ Assets/Scripts/ComputerItem.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class ComputerItem : InteractableObject{
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float minViewportSize;
+    private float newViewportSize;
+    private float startTime;
     public override void Interact(){
+        //StartCoroutine(zoomIn());
         onTriggerEvent.Invoke();
+    }
+    private IEnumerator zoomIn(){
+        startTime = Time.time;
+        while (Time.time < startTime + 0.5f){
+            float t = (Time.time - startTime) / 0.5f;
+            float newViewportSize = Mathf.Lerp(5.392406f, minViewportSize, t);
+            playerCamera.orthographicSize = newViewportSize;
+            yield return null;
+        }
+        playerCamera.orthographicSize = 5.392406f;
     }
 }
